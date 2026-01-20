a6cc75a109247b173b8c3b999a271ea267b82e47
taha60094 <taha.60094@iqra.edu.pk>
2026-01-21 02:03:18 +0500

    Initial commit: complete UML domain implementation

--- src/test/java/com/hotel/domain/GuestTest.java
+++ src/test/java/com/hotel/domain/GuestTest.java
@@ -8,17 +8,14 @@ class GuestTest {
 
     @Test
     void createGuest_validData_success() {
-        // Arrange & Act
         Guest guest = Guest.create(new Name("Sara"), new Address("Karachi"));
-
-        // Assert
         assertEquals("Sara", guest.getName().getValue());
     }
 
     @Test
     void createGuest_nullName_throwsException() {
-        // Arrange & Act & Assert
         assertThrows(NullPointerException.class,
                 () -> Guest.create(null, new Address("Islamabad")));
     }
 }
+
